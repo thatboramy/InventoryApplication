1b547de2a92fce07f155117e6c3f981e870b32c5
thatboramy <mcboramy@gmail.com>
2019-03-23 16:58:23 -0700

    Added one fragment

--- app/src/main/java/com/example/inventoryapplication/MainActivity.java
+++ app/src/main/java/com/example/inventoryapplication/MainActivity.java
@@ -36,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         //RECYCLERVIEW
         recyclerView =findViewById(R.id.recycler_view);

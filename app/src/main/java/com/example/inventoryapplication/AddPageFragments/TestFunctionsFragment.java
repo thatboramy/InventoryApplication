5b8856a88783b24fefb31aea618fc18528a7ba86
thatboramy <mcboramy@gmail.com>
2019-03-23 19:19:04 -0700

    Fragment onClickListener

--- app/src/main/java/com/example/inventoryapplication/AddPageFragments/TestFunctionsFragment.java
+++ app/src/main/java/com/example/inventoryapplication/AddPageFragments/TestFunctionsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.inventoryapplication.R;
 
@@ -12,10 +13,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class TestFunctionsFragment extends Fragment{
+    View currentView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        currentView = inflater.inflate(R.layout.fragment_test_functions, container, false);
+        Button save_button = currentView.findViewById(R.id.button_save);
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        return inflater.inflate(R.layout.fragment_test_functions, container, false);
+            }
+        });
+
+        return currentView;
     }
+
 }

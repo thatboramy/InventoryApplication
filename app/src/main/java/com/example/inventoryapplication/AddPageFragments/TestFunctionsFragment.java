056aa121010f6e6d757ccad39184276609f55d1c
skenneth <skenneth@asu.edu>
2019-03-25 11:40:37 -0700

    Created maximize feature to complement minimize.

--- app/src/main/java/com/example/inventoryapplication/AddPageFragments/TestFunctionsFragment.java
+++ app/src/main/java/com/example/inventoryapplication/AddPageFragments/TestFunctionsFragment.java
@@ -15,19 +15,31 @@ import androidx.fragment.app.Fragment;
 
 public class TestFunctionsFragment extends Fragment{
     View currentView;
-
+    boolean minimized = false;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         currentView = inflater.inflate(R.layout.fragment_test_functions, container, false);
         Button save_button = currentView.findViewById(R.id.button_save);
+
+        //section is open and saved button is not clicked causing it to be minimized
         save_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 minimize();
+                minimized = true;
             }
         });
-
+        //if section is minimized, maximize to show contents in fragment
+        currentView.setOnClickListener(new ViewGroup.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(minimized){
+                    maximize();
+                    minimized = false;
+                }
+            }
+        });
         return currentView;
     }
 
@@ -38,6 +50,16 @@ public class TestFunctionsFragment extends Fragment{
         input2.setVisibility(View.GONE);
         Button saveB = currentView.findViewById(R.id.button_save);
         saveB.setVisibility(View.GONE);
+
+    }
+
+    private void maximize() {
+        TextView input1 = currentView.findViewById(R.id.textView_itemName);
+        input1.setVisibility(View.VISIBLE);
+        TextView input2 = currentView.findViewById(R.id.textView_itemColor);
+        input2.setVisibility(View.VISIBLE);
+        Button saveB = currentView.findViewById(R.id.button_save);
+        saveB.setVisibility(View.VISIBLE);
 
     }
 

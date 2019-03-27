203dedfead3f7c3df6b488f5f9f7c143b3270617
skenneth <skenneth@asu.edu>
2019-03-27 12:11:59 -0700

    Updated layouts

--- app/src/main/java/com/example/inventoryapplication/AddActivity.java
+++ app/src/main/java/com/example/inventoryapplication/AddActivity.java
@@ -3,14 +3,20 @@ package com.example.inventoryapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+
+import com.example.inventoryapplication.AddPageFragments.ColorFragment;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class AddActivity extends AppCompatActivity {
-
+    Button saveButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
